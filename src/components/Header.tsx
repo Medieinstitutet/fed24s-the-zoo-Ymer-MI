e58a88ad74ac772bb3384ef4f75f4651c9ee9dfd
ymer94 <ymer_94@hotmail.com>
2025-06-11 17:41:00 +0200

    Updated: removed unused link in navigation.
    Changes to be committed:
    	modified:   src/components/Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,11 @@
-import { NavLink } from "react-router"
+import { NavLink } from 'react-router'
 
 export const Header = () => {
-    return <header className="flex justify-between items-center">
+    return <header className='flex justify-between items-center'>
         <h1>My Application</h1>
         <nav>
-            <ul className="flex gap-4">
-                <li><NavLink to='/'>Home</NavLink></li>
-                <li><NavLink to='/animals'>Animals</NavLink></li>
+            <ul className='flex gap-4'>
+                <li><NavLink to='/'>Animals</NavLink></li>
             </ul>
         </nav>
     </header>
