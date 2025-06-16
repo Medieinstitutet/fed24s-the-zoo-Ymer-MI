f58e211f79730cb9aea5d38dc2bd80a60afdbc6e
ymer94 <ymer_94@hotmail.com>
2025-06-16 17:27:58 +0200

    Updated: adjusted borders and button.
    Changes to be committed:
    	modified:   src/components/Error.tsx
    	modified:   src/styles/animalCard.scss

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,7 +1,9 @@
+import { Button } from './styled/Buttons'
+
 export const Error = () => {
     return <code>
         <h1>404 - Not Found</h1>
         <p>The page you are looking for does not exist.</p>
-        <a href='/'><button type='button'>Return home</button></a>
+        <Button type='button' onClick={ () => location.href = '/' }>Return home</Button>
     </code>
 }
