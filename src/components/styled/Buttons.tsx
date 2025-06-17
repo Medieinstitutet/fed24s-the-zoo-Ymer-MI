610bde4bba8e685c2b8778e3ee06fc87b2accda7
ymer94 <ymer_94@hotmail.com>
2025-06-17 13:17:39 +0200

    Updated styling of button.
    Changes to be committed:
    	modified:   src/components/styled/Buttons.tsx

--- src/components/styled/Buttons.tsx
+++ src/components/styled/Buttons.tsx
@@ -5,8 +5,25 @@ export const Button = styled.button`
     color: #222;
     background-color: #0f0;
     border-radius: .5rem;
+    transition: background 0.2s;
 
-    &:hover, &:pressed {
-        background-color: #4f4
+    &:disabled {
+        --background-greyed-out: #888;
+        --foreground-greyed-out: #ccc;
+
+        background-color: var(--background-greyed-out);
+        color: var(--foreground-greyed-out);
+        cursor: not-allowed;
+
+        &:hover, &:active {
+            background-color: var(--background-greyed-out);
+            color: var(--foreground-greyed-out);
+            text-decoration: none;
+        }
+    }
+    
+    &:hover, &:active {
+        background-color: #8f8;
+        text-decoration: underline;
     }
     `
